e6b25557a384dc2d10adc26dcdd976bedf64adc5
Ian Cargill <idcargill@gmail.com>
2021-12-15 21:03:40 -0800

    all test pass, readme update

--- pythonic_garage_band/Band.py
+++ pythonic_garage_band/Band.py
@@ -1,8 +1,11 @@
 class Band:
-  def __init__(self, name, members=None):
+  instances = []
+
+  def __init__(self, name, members=[]):
       self.name = name
       self.members = members
-
+      Band.instances.append(self.name)
+      
   def __str__(self):
       return f"The band {self.name}"
 
@@ -10,7 +13,17 @@ class Band:
       return f"Band instance. name={self.name}, members={self.members}"
 
   def play_solos(self):
-    pass
+    solos = []
+    for player in self.members:
+      solos.append(player.play_solo())
+    return solos
+
+  def add_band_member(self, musician):
+    self.members.append(musician)
+
+  @staticmethod
+  def to_list():
+    return Band.instances
 
 
 
@@ -19,26 +32,39 @@ class Musician:
     self.name = name
 
   def get_instrument(self):
-    return 'I got my instrument'
+    return f'{self.instrument}'
 
   def play_solo(self):
-    return 'doodley doodley doodley doodley'
+    return f'{self.solo}'
+      
+  def __str__(self):
+    return f'My name is {self.name} and I play {self.instrument}'
+
+  def __repr__(self):
+    return f'{self.__class__.__name__} instance. Name = {self.name}'
 
 
+class Guitarist(Musician):
+  def __init__(self, name):
+    super().__init__(name)
+    self.instrument = 'guitar'
+    self.solo = 'face melting guitar solo'
 
-class Guitarist:
-    pass
+class Bassist(Musician):
+    def __init__(self, name):
+      super().__init__(name)
+      self.instrument = 'bass'
+      self.solo = 'bom bom buh bom'
 
-class Bassist:
-    pass
-
-class Drummer:
-    pass
-
-
+class Drummer(Musician):
+    def __init__(self, name):
+      super().__init__(name)
+      self.instrument = 'drums'
+      self.solo = 'rattle boom crash'
 
 
-if __name__ == '__main__':
-  print('Hello Cleavland!')
-
+def from_file(file_path):
+  with open(file_path) as f:
+    bands = json.loads(f.read())
+    print(bands)
 
